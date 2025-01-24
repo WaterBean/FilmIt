216df7d2d45f8bd7b6c9de635dc2c3ad9f56e357
WaterBean <gkstnqls9@gmail.com>
2025-01-24 21:58:35 +0900

    [Chore] 기본 테마 라이트모드로 고정

--- FlimIt/FlimIt/SceneDelegate.swift
+++ FlimIt/FlimIt/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         let vc = ViewController()
-        vc.view.backgroundColor = .white
+        vc.view.backgroundColor = .systemBackground
         let nav = UINavigationController(rootViewController: vc)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
