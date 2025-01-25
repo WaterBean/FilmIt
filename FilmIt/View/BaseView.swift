67cbd9d3f582eb33914c475003b9ff2268d54adf
WaterBean <gkstnqls9@gmail.com>
2025-01-25 10:46:54 +0900

    [Feat] BaseView 함수 추가

--- FilmIt/View/BaseView.swift
+++ FilmIt/View/BaseView.swift
@@ -12,11 +12,24 @@ class BaseView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
+        configureHierarchy()
+        configureLayout()
+        configureView()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    /// view.addSubview()를 통해 계층 구조를 정의하는 함수
+    func configureHierarchy() { }
+    
+    /// 제약조건을 통해 뷰를 배치하는 함수
+    func configureLayout() { }
+    
+    /// view의 속성을 변경하는 함수
+    func configureView() { }
+    
     
 }
