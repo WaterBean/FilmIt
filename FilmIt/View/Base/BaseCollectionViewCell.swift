0758cf482a0e76819c7e2d471a7170f73ed08ca3
WaterBean <gkstnqls9@gmail.com>
2025-01-26 21:59:20 +0900

    [Feat] textColor 기본 appearnce를 하얀색으로 수정

---
+++ FilmIt/View/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BaseCollectionViewCell.swift
+//  FilmIt
+//
+//  Created by 한수빈 on 1/26/25.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ FilmIt/View/HeartButton.swift
@@ -0,0 +1,8 @@
+//
+//  HeartButton.swift
+//  FilmIt
+//
+//  Created by 한수빈 on 1/26/25.
+//
+
+import Foundation
