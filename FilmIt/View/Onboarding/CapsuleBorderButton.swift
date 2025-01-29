3d421d849b87cc0c2f57c42954cc1f264953c973
WaterBean <gkstnqls9@gmail.com>
2025-01-30 00:48:07 +0900

    [Refactor] 가독성을 위해 순서 변경

--- FilmIt/View/Onboarding/CapsuleBorderButton.swift
+++ FilmIt/View/Onboarding/CapsuleBorderButton.swift
@@ -10,13 +10,13 @@ import UIKit
 final class CapsuleBorderButton: BaseButton {
     
     convenience init(title: String) {
+        self.init(frame: .zero)
         var config = UIButton.Configuration.borderedProminent()
         config.cornerStyle = .capsule
         config.background.backgroundColor = .black
         config.background.strokeColor = .point
         config.baseForegroundColor = .point
-        config.attributedTitle = AttributedString(NSAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .semibold)]))
-        self.init(frame: .zero)
+        config.attributedTitle = AttributedString(NSAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .semibold), .foregroundColor: UIColor.point]))
         self.configuration = config
     }
     
