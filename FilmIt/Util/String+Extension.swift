51bf16b8ca60ed321e918dbff0c0414dad0dd957
WaterBean <gkstnqls9@gmail.com>
2025-01-31 16:31:43 +0900

    [Feat] imageBaseURL을 추가해주는 String extension 정의

---
+++ FilmIt/Util/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extension.swift
+//  FilmIt
+//
+//  Created by 한수빈 on 1/31/25.
+//
+
+import UIKit
+
+extension String {
+    func toImageURL() -> String {
+        return SecretManager.imageURL + self
+    }
+}
