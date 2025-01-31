c35f7c3780db51ec41f92021504277784f55dde3
WaterBean <gkstnqls9@gmail.com>
2025-01-31 14:19:12 +0900

    [Feat] 영화 검색 네트워크 응답 모델 정의

---
+++ FilmIt/Model/SearchResponse.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResponse.swift
+//  FilmIt
+//
+//  Created by 한수빈 on 1/30/25.
+//
+
+import Foundation
+
+struct SearchResponse: Codable {
+    let page: Int
+    let results: [Movie]
+    let totalPages: Int
+    let totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
